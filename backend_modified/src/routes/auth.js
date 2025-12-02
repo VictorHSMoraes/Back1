1de5f8c0332b4c10939417930e41ebdf31bfb2f1
Patoparamte <112555068+VictorHSMoraes@users.noreply.github.com>
2025-12-02 15:49:49 -0300

    Add route to get current user information

--- backend_modified/src/routes/auth.js
+++ backend_modified/src/routes/auth.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/authController');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.put("/update", auth, controller.update);
+router.get("/me", auth, controller.me);
 
 
 module.exports = router;
